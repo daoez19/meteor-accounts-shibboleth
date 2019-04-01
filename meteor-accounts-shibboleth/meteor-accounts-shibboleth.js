e62ce49c8e68a29e0d255eb6789156d3d91ba277
Nathaniel Wallace <nate.wallace@brms.wisc.edu>
2019-04-01 14:10:25 -0500

    remove mystery folder

--- meteor-accounts-shibboleth/meteor-accounts-shibboleth.js
+++
@@ -1,5 +0,0 @@
-// Write your package code here!
-
-// Variables exported by this module can be imported by other packages and
-// applications. See meteor-accounts-shibboleth-tests.js for an example of importing.
-export const name = 'meteor-accounts-shibboleth';
